5a6da53690016aa5fd44826e48921cd40dfaecdc
Maya <60146935+DinosaurPower@users.noreply.github.com>
2020-04-06 13:55:42 -0400

    Update sketch.js

--- Pages/Page 1/sketch.js
+++ Pages/Page 1/sketch.js
@@ -23,8 +23,8 @@ function draw() {
    rect(windowHeight/2, windowWidth/2, windowWidth/1.3, windowHeight/1.75); //second cool rectangle
 
   
-  gif_createImg.position(windowHeight/3, windowWidth/3); //I ad my gif and once it starts working I will set up its size and position
-   gif_createImg.size(windowWidth/1.3, windowHeight/2);
+  gif_createImg.position(windowHeight/3, windowWidth/5); //I ad my gif and once it starts working I will set up its size and position
+   gif_createImg.size(windowWidth/3, windowHeight/2.05);
   textSize(32);
   text.position(windowWidth/2, windowHeight/2);
 text('A young man stands in his bedroom. It just so happens that today, the 8th of April, 2020, is this young mans birthday. Though it was eighteen years ago he was given life, it is only today he will be given a name', 10, 30);
